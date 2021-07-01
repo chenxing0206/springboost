ae590e0d91d8139699aa1d7fcf06153994f135bc
fjhwscm00 <1QAZ2wsx>
2021-07-01 14:46:04 +0800

    sescond commit

--- demo/src/main/java/com/example/demo/bean/Person.java
+++ demo/src/main/java/com/example/demo/bean/Person.java
@@ -17,6 +17,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "myperson")
 public class Person {
 
+
     private String name;
     private int age;
     private int sex;
